863c42f1253ab15eb49831611ba387c4baf91074
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-08-09 10:24:39 -0500

    Display the getValues result on the client home page.

--- client/pages/api/services.ts
+++ client/pages/api/services.ts
@@ -1,20 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { ChannelCredentials } from "@grpc/grpc-js";
 import { KeyValueClient } from "ssr-grpc-proto-lib/models/key_value_service_grpc_pb";
-import { GetValuesRequest } from "ssr-grpc-proto-lib/models/key_value_service_pb";
+import {
+  GetValuesRequest,
+  GetValuesResponse,
+} from "ssr-grpc-proto-lib/models/key_value_service_pb";
 
 const service = new KeyValueClient(
+  // Don't prefix with http://
   "localhost:50051",
   ChannelCredentials.createInsecure(),
   {}
 );
 
 export const getService = () => ({
-  getValues: () => {
-    const request = new GetValuesRequest();
-    request.setKey("1234");
-    return service.getValues(request, (err, response) => {
-      console.log(err, response.getValue());
-    });
+  keyValueService: {
+    getValues: () => {
+      const request = new GetValuesRequest();
+      request.setKey("1234");
+      return new Promise<GetValuesResponse.AsObject>((resolve, reject) =>
+        service.getValues(request, (err, response) => {
+          if (err) return reject(err);
+          return resolve(response.toObject());
+        })
+      );
+    },
   },
 });
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,15 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { GetValuesResponse } from "ssr-grpc-proto-lib/models/key_value_service_pb";
 import styles from "../styles/Home.module.css";
 import { getService } from "./api/services";
 
-export default function Home() {
+type IProps = {
+  response: GetValuesResponse.AsObject;
+};
+
+export default function Home(props: IProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,46 +18,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <main className={styles.main}>Response: {props.response.value}</main>
 
       <footer className={styles.footer}>
         <a
@@ -69,11 +36,14 @@ export default function Home() {
   );
 }
 
-export async function getStaticProps(context: any) {
-  console.log("call static props");
-  const result = await getService().getValues();
+export const getServerSideProps: GetServerSideProps = async (
+  context
+): Promise<{ props: IProps }> => {
+  const response = await getService().keyValueService.getValues();
 
   return {
-    props: {},
+    props: {
+      response,
+    },
   };
-}
+};
