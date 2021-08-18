1d9fa12d311148d0ea146851edf4e2eb10df0359
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-08-18 10:12:05 -0500

    Remove Greeter / hello_world service definition in api/.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,7 @@
 import { Server, ServerCredentials } from "@grpc/grpc-js";
-import { GreeterService } from "ssr-grpc-proto-lib/models/hello_world_grpc_pb";
 import { InventoryManagmentService } from "ssr-grpc-proto-lib/models/inventory_management_grpc_pb";
 import { KeyValueService } from "ssr-grpc-proto-lib/models/key_value_service_grpc_pb";
 import { cassandraClient } from "./database/cassandra";
-import { Greeter } from "./services/hello_world_service";
 import { InventoryManagement } from "./services/inventory_management_service";
 import { KeyValue } from "./services/key_value_service";
 import { constructGrpcService } from "./services/types";
@@ -15,7 +13,6 @@ const server = new Server({
 });
 
 server.addService(KeyValueService, new KeyValue());
-server.addService(GreeterService, new Greeter());
 server.addService(
   InventoryManagmentService,
   constructGrpcService(new InventoryManagement())
