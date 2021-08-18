6ac46c3bbc04dd7e54b4855d3805fb5c7336e4ef
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-08-18 10:01:59 -0500

    Adds reads to the activity emulator.

--- cassandra/seed/modules/emulate_activity.ts
+++ cassandra/seed/modules/emulate_activity.ts
@@ -1,6 +1,6 @@
 import { Client } from "cassandra-driver";
-import { v4 } from "uuid";
 import faker from "faker";
+import { v4 } from "uuid";
 
 const createKeyspace = `CREATE KEYSPACE IF NOT EXISTS inventory \ 
                             WITH REPLICATION = { \
@@ -15,29 +15,74 @@ const createTable = `CREATE TABLE IF NOT EXISTS inventory.main ( \
                                     last_updated timestamp
                                 );`;
 
-async function createItems(client: Client): Promise<string> {
-  const id = v4();
-  await client.execute(
-    "INSERT INTO inventory.main (id, name, description, quantity, last_updated) VALUES (?, ?, ?, ?, ?)",
-    [
-      id,
-      faker.commerce.productName(),
-      faker.commerce.productDescription(),
-      faker.datatype.number(100),
-      Date.now(),
-    ],
-    {
-      prepare: true,
-    }
+async function createItems(client: Client, count: number): Promise<void> {
+  const promises: Array<Promise<boolean>> = new Array(count);
+
+  for (let i = 0; i < count; ++i) {
+    const id = v4();
+    promises[i] = client
+      .execute(
+        "INSERT INTO inventory.main (id, name, description, quantity, last_updated) VALUES (?, ?, ?, ?, ?)",
+        [
+          id,
+          faker.commerce.productName(),
+          faker.commerce.productDescription(),
+          faker.datatype.number(100),
+          Date.now(),
+        ],
+        {
+          prepare: true,
+        }
+      )
+      .then((_) => true);
+  }
+
+  const results = await Promise.allSettled(promises);
+
+  console.log(
+    "Successful writes:",
+    results.filter((res) => res.status === "fulfilled").length
   );
-  return id;
+
+  return;
 }
 
-async function deleteItem(client: Client, id: string): Promise<boolean> {
+async function readItems(client: Client, count: number): Promise<void> {
+  const { rows } = await client.execute(
+    "SELECT * FROM inventory.main LIMIT ?",
+    [count],
+    { prepare: true }
+  );
+
+  console.log("Simulating client reads over", rows.length, "rows.");
+
+  const promises: Array<Promise<boolean>> = new Array(rows.length);
+
+  for (let idx = 0; idx < rows.length; ++idx) {
+    promises[idx] = client
+      .execute(
+        "SELECT * FROM inventory.main WHERE id = ?",
+        [rows[idx]["id"].toString()],
+        {
+          prepare: true,
+        }
+      )
+      .then((_) => true);
+  }
+
+  const results = await Promise.allSettled(promises);
+  console.log(
+    "Read successes:",
+    results.filter((v) => v.status === "fulfilled").length
+  );
+
+  return;
+}
+
+async function deleteItems(client: Client, id: string): Promise<boolean> {
   return client
     .execute("DELETE FROM inventory.main WHERE id = ?;", [id])
-    .then((result) => true)
-    .catch((err) => false);
+    .then((result) => true);
 }
 
 async function handler(client: Client) {}
@@ -54,16 +99,15 @@ export async function emulateClientActivity(client: Client) {
 
   await client.execute(createTable);
 
-  const liveUUIDs = new Set<string>();
+  for (let i = 0; i < 100; ++i) {
+    const read = readItems(client, 1000);
+    const write = createItems(client, 1000);
 
-  for (let i = 0; i < 10000; ++i) {
-    let buffer = [];
-    for (let j = 0; j < 100; ++j) {
-      buffer.push(createItems(client));
-    }
-    await Promise.allSettled(buffer);
+    await read;
+    await write;
   }
 
-  console.log("Done emulating client activity");
+  console.log("Done emulating client activity.");
+
   return Promise.resolve(true);
 }
